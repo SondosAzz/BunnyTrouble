91bb6f51eae9eb07af19992a94856e5f60bf9259
SondosAzz <sondos.azzouz@gmail.com>
2015-04-16 14:00:03 +0300

    working on showing random numbers on the grid

---
+++ Assets/_Scripts/Element.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Element : MonoBehaviour 
+{
+	public bool number ;
+	public Sprite[] numberTextures;
+	void Start () {
+		//number = Random.value < .20;
+
+			//loadTexture (2);
+			//loadTexture (0);
+			loadTexture (3);
+
+	}
+	public void loadTexture(int counter){
+
+			GetComponent<SpriteRenderer> ().sprite = numberTextures [counter];
+
+
+
+		}
+
+
+
+
+
+
+
+
+}
+	
+
+
