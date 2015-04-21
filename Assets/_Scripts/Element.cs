190e80891363d2cb4a6062f41b841957eec368b6
SondosAzz <sondos.azzouz@gmail.com>
2015-04-21 09:10:36 +0300

    ...

--- Assets/_Scripts/Element.cs
+++ Assets/_Scripts/Element.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Element : MonoBehaviour 
 {
+<<<<<<< Updated upstream
 	public bool numberFive, numberSix, numberSeven;
 
 	public Sprite[] numberTextures;
@@ -22,6 +23,38 @@ public class Element : MonoBehaviour
 			loadTexture (2);
 		} else
 			loadTexture (3);
+=======
+	public bool numberS, numberE,numberW ,numberX ;
+
+	public Sprite[] numberTextures;
+	void Start () {
+		numberS = Random.value < .15;
+		numberE = Random.value < .15;
+		numberW = Random.value < .15;
+		numberX = Random.value < .15;
+	
+			if (numberS)
+		{
+			loadTexture (0);
+		} 
+		else if (numberE)
+		{
+			loadTexture (1);
+
+		} 
+		else if (numberW)
+		{
+			loadTexture (3);
+		}
+		else if (numberX) 
+		{
+			loadTexture (2);
+		} 
+		else 
+		{
+			loadTexture(4);
+		}
+>>>>>>> Stashed changes
 
 	
 
