b8d2d6f3146c0a978c80935b60e8728190009fa5
SondosAzz <sondos.azzouz@gmail.com>
2015-04-21 09:13:35 +0300

    Merge branch 'Grid'
    Conflicts:
    	Assembly-CSharp-vs.csproj
    	Assembly-CSharp.csproj
    	Assets/Scripts/Grid.cs
    	Assets/The Game/Scripts/Grid.cs
    	Assets/The Game/Tiles/grassCenter.png
    	Assets/The Game/Tiles/grassCenter.png.meta
    	Assets/Tiles/grass.png
    	Assets/Tiles/grassCenter.png.meta
    	Assets/_Graphics/Numbers.meta
    	Assets/_Graphics/Numbers/0.png.meta
    	Assets/_Graphics/grassCenter.png.meta
    	Assets/_Scenes/grid.unity
    	Assets/_Scenes/level1.unity
    	Assets/_Scripts/Grid.cs
    	BunnyTrouble-csharp.sln
    	BunnyTrouble.sln
    	BunnyTrouble.userprefs
    	Library/CurrentLayout.dwlt
    	Library/InspectorExpandedItems.asset
    	Library/ScriptAssemblies/Assembly-CSharp.dll
    	Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	Library/assetDatabase3
    	Library/expandedItems
    	Library/guidmapper
    	Library/metadata/00/00000000000000003000000000000000
    	Library/metadata/00/00000000000000004000000000000000
    	Library/metadata/00/00000000000000005000000000000000
    	Library/metadata/00/00000000000000006100000000000000
    	Library/metadata/00/00000000000000008000000000000000
    	Library/metadata/00/00000000000000009000000000000000
    	Library/metadata/00/0000000000000000b000000000000000
    	Library/metadata/00/0000000000000000c000000000000000
    	Library/metadata/04/04b3b3784f62749968784b0e554362b7
    	Library/metadata/04/04b3b3784f62749968784b0e554362b7.info
    	Library/metadata/07/071c61dfa9ab34bbbbe620a4f68458eb
    	Library/metadata/07/071c61dfa9ab34bbbbe620a4f68458eb.info
    	Library/metadata/0f/0f643fc0685234491a9de6daecab4316
    	Library/metadata/0f/0f643fc0685234491a9de6daecab4316.info
    	Library/metadata/54/54e578ad49589468e9b3a5ed47f2a7c5
    	Library/metadata/8a/8ae9e38c47c7c4d58940c4d3ddc04857
    	Library/metadata/8a/8ae9e38c47c7c4d58940c4d3ddc04857.info
    	Library/metadata/9c/9c6107852c6a34225a1fbb5cb65a7f65
    	Library/metadata/9c/9c6107852c6a34225a1fbb5cb65a7f65.info
    	Library/metadata/a5/a50f9769051c74191a512a6d17ca6923
    	Library/metadata/ae/ae0a63298096d44c38c4d6326501049c
    	Library/metadata/b7/b70e80708d35745859c7e86c806ffb79
    	Library/metadata/b7/b70e80708d35745859c7e86c806ffb79.info
    	Library/metadata/bc/bc1abbeb9d0fb4e429690d1a9c510fc4
    	Library/metadata/bc/bc1abbeb9d0fb4e429690d1a9c510fc4.info
    	Library/metadata/d8/d864ad31c606b42a6ab3af37435152c3
    	Library/metadata/d8/d864ad31c606b42a6ab3af37435152c3.info
    	Library/metadata/ef/ef7fbed2d8b604f7aac970a5fe18ebaa
    	Library/metadata/ef/ef7fbed2d8b604f7aac970a5fe18ebaa.info
    	Library/metadata/ff/ffba4e84326e643edaa129f561d0a721
    	Library/metadata/ff/ffba4e84326e643edaa129f561d0a721.info
    	ProjectSettings/TagManager.asset

---
+++ Assets/_Scripts/Element.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Element : MonoBehaviour 
+{
+<<<<<<< Updated upstream
+	public bool numberFive, numberSix, numberSeven;
+
+	public Sprite[] numberTextures;
+	void Start () {
+		numberFive = Random.value < .30;
+		numberSix = Random.value < .20;
+		numberSeven = Random.value < .20;
+	
+		if (numberFive) {
+
+			loadTexture (0);
+		} else if (numberSix) {
+			loadTexture (1);
+
+		} else if (numberSeven) {
+
+			loadTexture (2);
+		} else
+			loadTexture (3);
+=======
+	public bool numberS, numberE,numberW ,numberX ;
+
+	public Sprite[] numberTextures;
+	void Start () {
+		numberS = Random.value < .15;
+		numberE = Random.value < .15;
+		numberW = Random.value < .15;
+		numberX = Random.value < .15;
+	
+			if (numberS)
+		{
+			loadTexture (0);
+		} 
+		else if (numberE)
+		{
+			loadTexture (1);
+
+		} 
+		else if (numberW)
+		{
+			loadTexture (3);
+		}
+		else if (numberX) 
+		{
+			loadTexture (2);
+		} 
+		else 
+		{
+			loadTexture(4);
+		}
+>>>>>>> Stashed changes
+
+	
+
+	}
+	public void loadTexture(int counter){
+
+			GetComponent<SpriteRenderer> ().sprite = numberTextures [counter];
+
+
+
+		}
+
+
+
+
+
+
+
+
+}
+	
+
+
